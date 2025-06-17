d0e8a2be39f3573767cbe79ae8980bc51a2ea7d1
Johanna Söndergaard <johanna@MacBook-Air-som-tillhor-Johanna.local>
2025-06-17 21:21:49 +0200

    Add input for name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { Input } from './components/Input/Input';
-import { Text } from './components/Text/Text'
+import "./App.css";
+import { Input } from "./components/Input/Input";
+import { Text } from "./components/Text/Text";
+import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedName(inputValue);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-      <Text/>
+        <form onSubmit={handleSubmit}>
+          <Input value={inputValue} onChange={setInputValue} />
+          <button type="submit">Skicka</button>
+        </form>
+        <Text name={submittedName} />
       </header>
     </div>
   );
--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -1,50 +1,30 @@
 import React from "react";
 
-export function Text() {
-    return (
-        <p>
-        I dag er det [name]s fødselsdag
-        <br />
-        Hurra, hurra, hurra!
-        <br />
-        han/hun sikkert sig en gave får
-        <br />
-        som han/hun har ønsket sig i år
-        <br />
-        med dejlig chokolade og kager til.
-        <br />
-        <br />
-        Hvor smiler han/hun hvor er han/hun glad
-        <br />
-        Hurra, hurra, hurra!
-        <br />
-        men denne dag er også rar
-        <br />
-        for hjemme venter mor og far
-        <br />
-        med dejlig chokolade og kager til.
-        <br />
-        <br />
-        Og når han/hun hjem fra skole(/arbejde) går
-        <br />
-        Hurra, hurra, hurra!
-        <br />
-        så skal han/hun hjem og holde fest
-        <br />
-        og dem, som kommer med som gæst
-        <br />
-        får dejlig chokolade og kager til.
-        <br />
-        <br />
-        Til slut/sidst vi råber højt i kor:
-        <br />
-        Hurra, hurra, hurra!
-        <br />
-        Gid [name] længe leve må
-        <br />
-        og sine ønsker opfyldt få
-        <br />
-        med dejlig chokolade og kager til.
-        </p>
-    )
+export function Text({ name }) {
+  if (!name) return null;
+
+  return (
+    <p>
+      I dag er det {name}s fødselsdag<br />
+      Hurra, hurra, hurra!<br />
+      han/hun sikkert sig en gave får<br />
+      som han/hun har ønsket sig i år<br />
+      med dejlig chokolade og kager til.<br />
+      Hvor smiler han/hun hvor er han/hun glad<br />
+      Hurra, hurra, hurra!<br />
+      men denne dag er også rar<br />
+      for hjemme venter mor og far<br />
+      med dejlig chokolade og kager til.<br />
+      Og når han/hun hjem fra skole(/arbejde) går<br />
+      Hurra, hurra, hurra!<br />
+      så skal han/hun hjem og holde fest<br />
+      og dem, som kommer med som gæst<br />
+      får dejlig chokolade og kager til.<br />
+      Til slut/sidst vi råber højt i kor:<br />
+      Hurra, hurra, hurra!<br />
+      Gid {name} længe leve må<br />
+      og sine ønsker opfyldt få<br />
+      med dejlig chokolade og kager til.
+    </p>
+  );
 }
